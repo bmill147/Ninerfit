0b4bd60d9b0de6375bed0d5e56110aba6df75a7a
Justin Cooper Ray <jray72@uncc.edu>
2025-05-07 16:24:20 -0400

    test

--- src/server.ts
+++ src/server.ts
@@ -4,13 +4,7 @@ import path from 'path';
 import dotenv from 'dotenv';
 import usersRouter from './routes/users';
 import workoutsRouter from './routes/workouts';
-<<<<<<< HEAD
 import exercisesRouter from './routes/exercises'; // add if not already
-=======
-
-app.use('/workouts', workoutsRouter);
-
->>>>>>> parent of dfc873a (updates to backend)
 
 dotenv.config();
 const app = express();
@@ -19,19 +13,10 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-// 👇 serve static files from public
-app.use(express.static(path.join(__dirname, '../public')));
-
-app.get('/', (_req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
-});
 
 app.use('/users', usersRouter);
-<<<<<<< HEAD
 app.use('/workouts', workoutsRouter);
 app.use('/exercises', exercisesRouter); // add if not yet
-=======
->>>>>>> parent of dfc873a (updates to backend)
 
 app.listen(PORT, () => {
   console.log(`Server is listening on http://localhost:${PORT}`);
